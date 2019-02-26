c2b6ae3871217a3e4f9ea762d4117951ed4e250a
Cœt <33434015+aisquad@users.noreply.github.com>
2019-02-26 12:59:33 +0100

    nuevas pruebas

--- php/pruebas/pruebas.php
+++ php/pruebas/pruebas.php
@@ -1,6 +1,9 @@
 <?php
-    $almacen->anyadir_producto(8, 'Trufa 8');
-    $almacen->incrementar_stock(4);
-    $almacen->incrementar_stock(4);
-    $almacen->incrementar_stock(4);
+    function espera_1 () {
+        global $almacen;
+        $almacen->anyadir_producto(8, 'Trufa 8');
+        $almacen->incrementar_stock(4);
+        $almacen->incrementar_stock(4);
+        $almacen->incrementar_stock(4);
+    }
 ?>
--- php/pruebas/pruebas2.php
+++ php/pruebas/pruebas2.php
@@ -1,6 +1,9 @@
 <?php
-    $almacen->anyadir_producto(9, 'Trufa 9');
-    $almacen->incrementar_stock(4);
-    $almacen->incrementar_stock(4);
-    $almacen->incrementar_stock(4);
+    function espera_2 () {
+        global $almacen;
+        $almacen->anyadir_producto(9, 'Trufa 9');
+        $almacen->incrementar_stock(4);
+        $almacen->incrementar_stock(4);
+        $almacen->incrementar_stock(4);
+    }
 ?>
