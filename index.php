e54811fef80390fe75a2156fc2504871f51bd591
Cœt <33434015+aisquad@users.noreply.github.com>
2019-02-26 12:55:17 +0100

    more calls

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
     require_once './php/modelo/almacen.php';
-	$almacen = Almacen::instanciar();
-    $almacen->obtener_productos();
+    require_once './php/modelo/cesta.php';
+    require_once './php/modelo/clientela.php';
     require_once './php/pruebas/pruebas.php';
     require_once './php/pruebas/pruebas2.php';
-    var_dump($almacen->devolver_productos());
+    $almacen = Almacen::instanciar();
+    $almacen->cargar_productos();
+    espera_1();
+    espera_2();
+    echo 'ALMACEN<br>';
+    var_dump($almacen->obtener_productos());
     echo '<br>' . "#{$almacen->talla()}";
+    $cesta = Cesta::instanciar();
+    $cesta->cargar_articulos();
+    echo '<br><br>CESTA<br>';
+    var_dump($cesta->pasar_cesta());
+    echo '<br>';
+    var_dump($cesta->articulos_a_vector());
+    echo '<br><br>CLIENTES<br>';
+    var_dump($cesta->articulos_a_json());
+    $clientes = Clientes::instanciar();
+    $clientes->cargar_clientes();
+    echo '<br>';
+    var_dump($clientes->obtener_clientes());
 ?>
